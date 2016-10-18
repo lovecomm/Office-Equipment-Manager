4870d59c1bc67c6b0658cfc960311aed62404655
Logan Call <logancall.dev@gmail.com>
2016-10-18 12:42:09 -0600

    made desktop version of navigation

--- app/components/Navigation/Navigation.js
+++ app/components/Navigation/Navigation.js
@@ -2,34 +2,59 @@ import React, { PropTypes } from 'react'
 import { Link } from 'react-router'
 import { IconMenu, MenuItem, MenuDivider } from 'react-toolbox/lib/menu'
 import { AppBar } from 'react-toolbox/lib/app_bar'
-import { title, menuItems } from './styles.scss'
+import { title, menuItems, menuItemsDesktop, appBar } from './styles.scss'
 import { container } from 'sharedStyles/layout'
 
 function MenuItems (props) {
 	if (props.isAuthed) {
-		return (<IconMenu className={menuItems} icon='reorder' menuRipple={true}>
-			<MenuItem caption={'Items'}
-				onClick={() => {
-					props.router.push('/')
-					props.changeActiveCards('items')
-				}}
-				className={(() => props.activeCards === 'items' ? 'active' : '')()} icon='view_agenda' />
-			<MenuItem caption={'People'}
-				onClick={() => {
-					props.router.push('/people')
-					props.changeActiveCards('people')
-				}}
-				className={(() => props.activeCards === 'people' ? 'active' : '')()} icon='people' />
-			<MenuItem caption={'Hardware'}
-				onClick={() => {
-					props.router.push('/hardware')
-					props.changeActiveCards('hardware')
-				}}
-				className={(() => props.activeCards === 'hardware' ? 'active' : '')()} icon='laptop' />
-			<MenuDivider />
-			<MenuItem icon='close' caption={'Sign Out'}
-				onClick={() => props.router.push('/logout')} />
-		</IconMenu>)
+		if (window.outerWidth <= 900) { // dropdown/mobile menu
+			return (<IconMenu className={menuItems} icon='reorder' menuRipple={true}>
+				<MenuItem caption={'Items'}
+					onClick={() => {
+						props.router.push('/')
+						props.changeActiveCards('items')
+					}}
+					className={(() => props.activeCards === 'items' ? 'active' : '')()} icon='view_agenda' />
+				<MenuItem caption={'People'}
+					onClick={() => {
+						props.router.push('/people')
+						props.changeActiveCards('people')
+					}}
+					className={(() => props.activeCards === 'people' ? 'active' : '')()} icon='people' />
+				<MenuItem caption={'Hardware'}
+					onClick={() => {
+						props.router.push('/hardware')
+						props.changeActiveCards('hardware')
+					}}
+					className={(() => props.activeCards === 'hardware' ? 'active' : '')()} icon='laptop' />
+				<MenuDivider />
+				<MenuItem icon='close' caption={'Sign Out'}
+					onClick={() => props.router.push('/logout')} />
+			</IconMenu>)
+		} else { // desktop menu
+			return <div className={menuItemsDesktop}>
+				<MenuItem caption={'Items'}
+					onClick={() => {
+						props.router.push('/')
+						props.changeActiveCards('items')
+					}}
+					className={(() => props.activeCards === 'items' ? 'active' : '')()}/>
+				<MenuItem caption={'People'}
+					onClick={() => {
+						props.router.push('/people')
+						props.changeActiveCards('people')
+					}}
+					className={(() => props.activeCards === 'people' ? 'active' : '')()}/>
+				<MenuItem caption={'Hardware'}
+					onClick={() => {
+						props.router.push('/hardware')
+						props.changeActiveCards('hardware')
+					}}
+					className={(() => props.activeCards === 'hardware' ? 'active' : '')()}/>
+				<MenuItem caption={'Sign Out'}
+					onClick={() => props.router.push('/logout')} />
+			</div>
+		}
 	} else {
 		return <span></span>
 	}
@@ -37,7 +62,7 @@ function MenuItems (props) {
 
 export default function Navigation (props, context) {
 	return (
-		<AppBar>
+		<AppBar className={appBar}>
 			<div className={container}>
 				<Link to='/' className={title}>{'Equipment Manager'}</Link>
 				<MenuItems {...props} router={context.router} />
@@ -48,17 +73,17 @@ export default function Navigation (props, context) {
 
 
 Navigation.propTypes = MenuItems.propTypes = {
- isAuthed: PropTypes.bool.isRequired,
- changeSortOrder: PropTypes.func.isRequired,
- sortFeedCreationDate: PropTypes.func.isRequired,
- sortFeedPurchaseDate: PropTypes.func.isRequired,
- sortFeedLastName: PropTypes.func.isRequired,
- sortFeedFirstName: PropTypes.func.isRequired,
- sortFeedHardware: PropTypes.func.isRequired,
- changeActiveCards: PropTypes.func.isRequired,
- activeCards: PropTypes.string.isRequired,
+	isAuthed: PropTypes.bool.isRequired,
+	changeSortOrder: PropTypes.func.isRequired,
+	sortFeedCreationDate: PropTypes.func.isRequired,
+	sortFeedPurchaseDate: PropTypes.func.isRequired,
+	sortFeedLastName: PropTypes.func.isRequired,
+	sortFeedFirstName: PropTypes.func.isRequired,
+	sortFeedHardware: PropTypes.func.isRequired,
+	changeActiveCards: PropTypes.func.isRequired,
+	activeCards: PropTypes.string.isRequired,
 }
 
 Navigation.contextTypes = MenuItems.contextTypes = {
- router: PropTypes.object.isRequired,
+	router: PropTypes.object.isRequired,
 }
