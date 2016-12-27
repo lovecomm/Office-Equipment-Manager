cd35f2ebf89f121e062ca42ad7caca2bcc8e39fb
Logan Call <logancall.dev@gmail.com>
2016-12-27 10:04:13 -0700

    style updates

--- app/components/Logout/Logout.js
+++ app/components/Logout/Logout.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router'
+import { container } from './styles.scss'
 
 export default function Logout (props) {
 	return (
-		<div>
+		<div className={container}>
 			<p>You have successfuly signed out.</p>
 			<p><Link to='/auth'>Log back in?</Link></p>
 		</div>
