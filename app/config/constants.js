26493ed0f6c5695bcbc6ec9f714cb4e986ff050c
Logan Call <logancall.dev@gmail.com>
2017-01-03 11:31:50 -0700

    removed dist and firebase app info from repo

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,11 +1,5 @@
 import firebase from 'firebase'
-
-const config = {
-	apiKey: 'AIzaSyA4AJblcbzujdSNmffpLimkNGxHG-aXkPA',
-	authDomain: 'equipment-manager-5a647.firebaseapp.com',
-	databaseURL: 'https://equipment-manager-5a647.firebaseio.com',
-	storageBucket: 'equipment-manager-5a647.appspot.com',
-}
+import { config } from 'config/conf'
 
 firebase.initializeApp(config)
 
